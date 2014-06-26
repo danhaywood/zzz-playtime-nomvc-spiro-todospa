685b0832c5f893356ef2c4eeeb0c96e2e180698b
scascarini <scascarini@nakedobjects.org>
2014-06-26 16:50:56 +0100

    complete action

--- scripts/todoapp.controllers.js
+++ scripts/todoapp.controllers.js
@@ -5,15 +5,27 @@
 // tested
 var ToDoApp;
 (function (ToDoApp) {
-    ToDoApp.app.controller('HomeController', function ($scope, repLoader, transformer) {
-        transformer.transform("http://localhost:43055/rest/services/Domain.ToDoItems/actions/NotYetComplete/invoke", Spiro.ActionResultRepresentation).then(function (todoItems) {
+    ToDoApp.app.controller('HomeController', function ($scope, repLoader, transformer, $location) {
+        transformer.transform(Spiro.appPath + "/services/Domain.ToDoItems/actions/NotYetComplete/invoke", Spiro.ActionResultRepresentation).then(function (todoItems) {
             $scope["todoItems"] = todoItems;
         });
+
+        $scope["complete"] = function (url) {
+            var a = url.substring(1, url.length);
+
+            var action = new Spiro.ActionResultRepresentation({});
+            action.method = "PUT";
+            action.hateoasUrl = Spiro.appPath + "/objects" + a + "/actions/Completed/invoke";
+
+            repLoader.populate(action).then(function (ar) {
+                $location.url("");
+            });
+        };
     });
 
     ToDoApp.app.controller('ToDoItemController', function ($scope, $routeParams, transformer) {
         var id = $routeParams.tdid;
-        var url = "http://localhost:43055/rest/objects/Domain.ToDoItem/" + id;
+        var url = Spiro.appPath + "/objects/Domain.ToDoItem/" + id;
 
         transformer.transform(url, Spiro.DomainObjectRepresentation).then(function (fo) {
             $scope["todoItem"] = fo;
--- scripts/todoapp.controllers.ts
+++ scripts/todoapp.controllers.ts
@@ -7,16 +7,29 @@
 module ToDoApp {
 
   
-    app.controller('HomeController', ($scope: ng.IScope, repLoader : Spiro.Angular.IRepLoader, transformer : ITransformer) => {
-        transformer.transform("http://localhost:43055/rest/services/Domain.ToDoItems/actions/NotYetComplete/invoke",
+    app.controller('HomeController', ($scope: ng.IScope, repLoader : Spiro.Angular.IRepLoader, transformer : ITransformer, $location : ng.ILocationService) => {
+        transformer.transform(Spiro.appPath + "/services/Domain.ToDoItems/actions/NotYetComplete/invoke",
             Spiro.ActionResultRepresentation).then((todoItems: any) => {
             $scope["todoItems"] = todoItems;
         });
+
+        $scope["complete"] = function(url: string) {
+
+            var a = url.substring(1, url.length);
+
+            var action = new Spiro.ActionResultRepresentation({});
+            action.method = "PUT";
+            action.hateoasUrl = Spiro.appPath +  "/objects" + a + "/actions/Completed/invoke";
+
+            repLoader.populate(action).then((ar: Spiro.ActionResultRepresentation) => {
+                $location.url("");
+            });
+        };
     });
 
     app.controller('ToDoItemController', ($scope: ng.IScope, $routeParams, transformer: ITransformer) => {
         var id = $routeParams.tdid;
-        var url = "http://localhost:43055/rest/objects/Domain.ToDoItem/" + id;
+        var url = Spiro.appPath + "/objects/Domain.ToDoItem/" + id;
 
         transformer.transform(url, Spiro.DomainObjectRepresentation).then((fo : any) => {
             $scope["todoItem"] = fo;
